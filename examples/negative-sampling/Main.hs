d9565979a5c86c370cd09d177d381f62684d6147
AdLucem <ghosalatreyee@gmail.com>
2019-06-26 22:53:02 +0530

    began negative sampling

---
+++ examples/negative-sampling/Main.hs
@@ -0,0 +1,15 @@
+
+
+-- consider a pair (w, c) of word and context
+data Pair = (String, String)
+
+-- consider the training data as a list of size 'n' windows
+-- with each word being in the center of it's context window
+data TrainingData = [[String]]
+
+-- probability that pair in training data, given parameters theta
+-- for a distribution
+p theta = 
+
+main :: IO ()
+main = putStrLn "hello"
